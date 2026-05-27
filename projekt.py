1f7cac3122280eb5103c7008127c0a42da8589d5
tasnim129 <tasnim.haouala@gmail.com>
2026-05-27 03:13:57 +0200

    darstellug von train und test

--- projekt.py
+++ projekt.py
@@ -1,11 +1,35 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
 
 # Dataset laden
 df = pd.read_csv("movie_metadata.csv", 
                  sep=",")
+ #data cleaning
+print(df.head())
+print(df.info())
+print(df.describe())
+print(df.columns)
 
+X=df.drop("imdb_score", axis=1)
+y=df["imdb_score"].copy()
+X_train_full, y_train_full, X_test, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+X_train,y_train,X_val,y_val=train_test_split(
+    X_train_full,
+    y_train_full,
+    test_size=0.25
+    random_state=42
+)
+
+#um zu verifizieren:
+#print()
 # Größe der Grafik
 plt.figure(figsize=(12,8))
 
